26ac28b9f92c0f152ecc22ecd1bb471758ffb7cf
sumitkumar2299 <sumitofficialme@gmail.com>
2025-05-16 16:06:54 +0530

    starting arary 2d

---
+++ ARRAY_2D/12dArray.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int arr[3][3];
+    // rows -> 3     0- 2 
+    // columns -> 3   0-2
+
+
+    // bina initialize kiye array ke value ko print karoge 
+    // toh array garbage value throw karega. 
+
+    // cout<<arr[0][0];
+    // arr[0][0] = 4;
+    // cout<<arr[0][0];
+// 2d arrays are also arrays of array 
+// 
+    // int arr[4][4] = {{1,2,3},{4,5,6},{7,8,9},{4,8,9}};
+    // cout<<arr[0][1];
+
+
+    // another way of doing this. 
+    int arr[4][4] = {1,2,3,4,5,6,7,8,9,1,0,11,12};
+    cout<<arr[0][1];
+
+
+return 0;
+}
