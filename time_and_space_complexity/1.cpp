55bd1d4e717e566f1ec2287467a9675121c7a39c
sumitkumar2299 <sumitofficialme@gmail.com>
2025-08-21 17:37:06 +0530

    solving time and complexity problems

---
+++ time_and_space_complexity/1.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    for(int i =1; i<=5; i++){
+        for(int j = 1; j<=1; j++){
+            cout<<"sumit";
+        }
+    }
+    
+    // the time complexity of these loops are big oh of n square 
+
+    
+return 0;
+}
